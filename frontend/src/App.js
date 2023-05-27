df8359b0db69f64e84d5537def182bd014106d84
zhoffman1 <zfh1@pitt.edu>
2023-05-26 20:24:45 -0400

    Turns out... you need reportWebVitals.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,27 @@
 import './App.css';
 import { getter } from './Utils';
 
-function App() {
+const App = () => {
     return (
-    <div className="App">
-        <div>
-            <h1>Login</h1>
+        <div className="App">
+            <div>
+                <h1>Login</h1>
+            </div>
+            <div>
+                <form>
+                    <label for="fname">{ "First name:" }</label>
+                    <input type="text" id="fname" name="fname"/>
+                    <div></div>
+                    <label for="lname">{ "Last name:" }</label>
+                    <input type="text" id="lname" name="lname"/>
+                    <div></div>
+                    <input type="submit" value="Submit"/>
+                </form>
+            </div>
+            <div>
+                { /* Account registration */ }
+            </div>
         </div>
-        <div>
-            <form>
-                <label for="fname">{ "First name:" }</label>
-                <input type="text" id="fname" name="fname"/>
-                <div></div>
-                <label for="lname">{ "Last name:" }</label>
-                <input type="text" id="lname" name="lname"/>
-                <div></div>
-                <input type="submit" value="Submit"/>
-            </form>
-        </div>
-        <div>
-            { /* Account registration */ }
-        </div>
-    </div>
     );
 }
 
